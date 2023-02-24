10c4643007c689506cdcf9b90f2dcd586ca906a7
Jayson Webb <jayson.m.webb@gmail.com>
2023-02-24 14:43:36 -0700

    Add files via upload
    added to main.java code to read the data to generate a prediction from a csv file

---
+++ README.md
@@ -0,0 +1,5 @@
+#Java compile command
+javac -cp h2o-genmodel.jar -J-Xms2g main.java
+
+#Run command
+java -cp .:h2o-genmodel.jar main
--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 import hex.genmodel.easy.RowData;
 import hex.genmodel.easy.EasyPredictModelWrapper;
 import hex.genmodel.easy.prediction.*;
@@ -6,19 +7,26 @@ import hex.genmodel.MojoModel;
 
 public class main {
   public static void main(String[] args) throws Exception {
+
     EasyPredictModelWrapper.Config config = new EasyPredictModelWrapper.Config()
         .setModel(MojoModel.load("GBM_grid_1_AutoML_1_20230217_161120_model_266.zip"))
         .setEnableLeafAssignment(true)
         .setEnableContributions(true);
     EasyPredictModelWrapper model = new EasyPredictModelWrapper(config);
 
+    //get our data to generate the prediction from a file
+    Scanner sc = new Scanner(new File("insurance_predict.csv"));  
+    sc.useDelimiter(",");   //sets the delimiter pattern
+
     RowData row = new RowData();
-    row.put("age", "59");
-    row.put("sex", "male");
-    row.put("bmi", "27.6");
-    row.put("children", "0");
-    row.put("smoker", "no");
-    row.put("region", "southwest");
+    row.put("age", sc.next().trim());
+    row.put("sex", sc.next().trim());
+    row.put("bmi", sc.next().trim());
+    row.put("children", sc.next().trim());
+    row.put("smoker", sc.next().trim());
+    row.put("region", sc.next().trim());
+    System.out.println(row);
+    sc.close();
 
     RegressionModelPrediction p = model.predictRegression(row);
 
